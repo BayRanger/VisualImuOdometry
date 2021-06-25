36f3754a1bda22c81f246f48643b24523ee6ebbd
grobothero <hechangxin@gmail.com>
2021-06-25 09:19:01 +0200

    parse timestamp data

--- VINS-Course/test/run_sim.cpp
+++ VINS-Course/test/run_sim.cpp
@@ -23,32 +23,7 @@ string sConfig_path = "../config/";
 
 std::shared_ptr<System> pSystem;
 
-void PubImuData()
-{
-	string sImu_data_file = sConfig_path + "MH_05_imu0.txt";
-	cout << "1 PubImuData start sImu_data_filea: " << sImu_data_file << endl;
-	ifstream fsImu;
-	fsImu.open(sImu_data_file.c_str());
-	if (!fsImu.is_open())
-	{
-		cerr << "Failed to open imu file! " << sImu_data_file << endl;
-		return;
-	}
 
-	std::string sImu_line;
-	double dStampNSec = 0.0;
-	Vector3d vAcc;
-	Vector3d vGyr;
-	while (std::getline(fsImu, sImu_line) && !sImu_line.empty()) // read imu data
-	{
-		std::istringstream ssImuData(sImu_line);
-		ssImuData >> dStampNSec >> vGyr.x() >> vGyr.y() >> vGyr.z() >> vAcc.x() >> vAcc.y() >> vAcc.z();
-		cout << "Imu t: " << fixed << dStampNSec << " gyr: " << vGyr.transpose() << " acc: " << vAcc.transpose() << endl;
-		pSystem->PubImuData(dStampNSec / 1e9, vGyr, vAcc);
-		usleep(5000*nDelayTimes);
-	}
-	fsImu.close();
-}
 
 void ParseImuData()
 {
@@ -76,6 +51,75 @@ void ParseImuData()
 		usleep(5000*nDelayTimes);
 	}
 	fsImu.close();
+}
+vector<double> ParseImageTimestamp()
+{
+	vector<double> ts_vec;
+
+	string sImgTImestampFile = sConfig_path + "cam_pose.txt";
+
+	cout << "Parse timestamp file: " << sImgTImestampFile << endl;
+
+	ifstream fsCam;
+	fsCam.open(sImgTImestampFile.c_str());
+	if (!fsCam.is_open())
+	{
+		cerr << "Failed to open camera file! " << sImgTImestampFile << endl;
+		return ts_vec;
+	}
+	std::string ts_line;
+	double StampSec = 0.0;
+ 	while (std::getline(fsCam, ts_line) && !ts_line.empty()) // read imu data
+	{
+		std::istringstream ssTimestamp(ts_line);
+		ssTimestamp >> StampSec;
+		cout << "timestamp: " <<StampSec << endl;
+		ts_vec.push_back(StampSec);
+		//pSystem->PubImuData(StampNSec, vGyr, vAcc);
+		usleep(5000*nDelayTimes);
+	}
+	fsCam.close();
+
+	return ts_vec;
+}
+void ParseImageData()
+{
+	string sImage_file = sConfig_path + "MH_05_cam0.txt";
+
+	cout << "1 PubImageData start sImage_file: " << sImage_file << endl;
+
+	ifstream fsImage;
+	fsImage.open(sImage_file.c_str());
+	if (!fsImage.is_open())
+	{
+		cerr << "Failed to open image file! " << sImage_file << endl;
+		return;
+	}
+
+	std::string sImage_line;
+	double dStampNSec;
+	string sImgFileName;
+	
+	// cv::namedWindow("SOURCE IMAGE", CV_WINDOW_AUTOSIZE);
+	while (std::getline(fsImage, sImage_line) && !sImage_line.empty())
+	{
+		std::istringstream ssImuData(sImage_line);
+		ssImuData >> dStampNSec >> sImgFileName;
+		// cout << "Image t : " << fixed << dStampNSec << " Name: " << sImgFileName << endl;
+		string imagePath = sData_path + "cam0/data/" + sImgFileName;
+
+		Mat img = imread(imagePath.c_str(), 0);
+		if (img.empty())
+		{
+			cerr << "image is empty! path: " << imagePath << endl;
+			return;
+		}
+		pSystem->PubImageData(dStampNSec / 1e9, img);
+		// cv::imshow("SOURCE IMAGE", img);
+		// cv::waitKey(0);
+		usleep(50000*nDelayTimes);
+	}
+	fsImage.close();
 }
 
 void PubImageData()
@@ -131,6 +175,7 @@ int main(int argc, char **argv)
 	sData_path ="useless junk";
 	//sConfig_path = argv[2];
 	sConfig_path = "../sensor_data/";//TODO change it 
+	ParseImageTimestamp();
 
 
 	pSystem.reset(new System(sConfig_path));
